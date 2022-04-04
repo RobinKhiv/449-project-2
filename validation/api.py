7b51a4087a1e28d4fb257ceb5b4edaa6a5cec7bb
P-R-I-16 <100308683+P-R-I-16@users.noreply.github.com>
2022-04-03 23:32:37 -0700

    Update api.py

--- validation/api.py
+++ validation/api.py
@@ -10,7 +10,7 @@ import logging.config
 import sqlite3
 import typing
 
-from datetime import datetime
+import uvicorn
 from fastapi import FastAPI, Depends, Response, HTTPException, status
 from pydantic import BaseModel, BaseSettings
 
@@ -23,10 +23,9 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 
-class words(BaseModel):
+class Words(BaseModel):
     id: int
     word: str
-
 
 
 def get_db():
@@ -45,22 +44,66 @@ app: FastAPI = FastAPI()
 logging.config.fileConfig(settings.logging_config)
 
 
-@app.get("/validate/{word}")
+@app.post("/validate/guess/{word}")
 def validate_word(
-    word: str, response: Response, db: sqlite3.Connection = Depends(get_db)
+        word: str, response: Response, db: sqlite3.Connection = Depends(get_db)
 ):
     if len(word) != 5:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="allowed word length is 5")
 
-    cur = db.execute("SELECT * FROM words WHERE word = ? LIMIT 1", [word])
+    cur = db.execute("SELECT * FROM words WHERE UPPER(word) = UPPER(?) LIMIT 1", [word])
     words = cur.fetchall()
     if not words:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="entered word is not a valid word"
+            status_code=status.HTTP_404_NOT_FOUND, detail={"entered word is not a valid word":word}
         )
-    return {"words": words}
+    return {"isValidWord": "true"}
+
+
+@app.post("/add/guess", status_code=status.HTTP_201_CREATED)
+def create_word(
+        words: Words, response: Response, db: sqlite3.Connection = Depends(get_db)
+):
+    w = dict(words)
+    try:
+        cur = db.execute(
+            """
+            INSERT INTO WORDS(wordid, word)
+            VALUES(:id,:word)
+            """,
+            w,
+        )
+        db.commit()
+    except sqlite3.IntegrityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={"type": type(e).__name__, "msg": str(e)},
+        )
+    w["id"] = cur.lastrowid
+    response.headers["Location"] = f"/words/{w['id']}"
+    return w
+
+
+@app.post("/remove/guess", status_code=status.HTTP_200_OK)
+def remove_word(
+        words: Words, response: Response, db: sqlite3.Connection = Depends(get_db)
+):
+    w = dict(words)
+    cur = db.execute(
+        """
+           DELETE  FROM words WHERE wordid = :id and word=:word
+            """,
+        w,
+    )
+    if cur.rowcount == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"no record exists for the id and word": w}
+        )
+    db.commit()
+    return w
 
 
 if __name__ == "__main__":
-    uvicorn.run("validate:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, log_level="info")
